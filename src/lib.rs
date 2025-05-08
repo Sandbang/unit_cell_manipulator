e260a61072b249df613aa9133260d76eb55596b8
Sandbang <michael.serokurov@gmail.com>
2025-05-07 23:07:02 -0700

    added rotate

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 
 
-use nalgebra::{Matrix, Rotation3, Vector3, Vector4};
-
+use nalgebra::{Matrix, Matrix3, Rotation3, Vector3, Vector4};
 pub struct Atom {
     pub position: Vector3<f32>,
     pub number: i16
@@ -12,16 +11,15 @@ pub struct UnitCell{
     pub vec2: Vector3<f32>,
     pub vec3: Vector3<f32>,
     pub atoms: Vec<Atom>,
-    pub atom_count: usize,
+    pub base_atom_count: usize,
     pub basis: bool,
 }
 impl UnitCell {
     pub fn print(&mut self){
-        println!("{}", self.vec1);
-        println!("{}", self.vec2);
-        println!("{}", self.vec3);
+        let m = Matrix3::from_columns(&[self.vec1, self.vec2, self.vec3]).transpose();
+        println!("{}", m);
         for i in 0..self.atoms.len(){
-            println!("{}", self.atoms[i].number);
+            print!("{}:", self.atoms[i].number);
             println!("{}", self.atoms[i].position);
         }
     }
@@ -32,7 +30,7 @@ impl UnitCell {
 
         // For first extension dimension
         for i in 1..a{
-            for j in 0..self.atom_count{
+            for j in 0..self.base_atom_count{
                 let new_atom = Atom{
                     position: self.atoms[j].position + self.vec1 * i as f32,
                     number: self.atoms[j].number
@@ -42,7 +40,7 @@ impl UnitCell {
         }
         // For second extension dimension
         for i in 1..b{
-            for j in 0..self.atom_count{
+            for j in 0..self.base_atom_count{
                 let new_atom = Atom{
                     position: self.atoms[j].position + self.vec2 * i as f32,
                     number: self.atoms[j].number
@@ -52,7 +50,7 @@ impl UnitCell {
         }
         // For third extension dimension
         for i in 1..c{
-            for j in 0..self.atom_count{
+            for j in 0..self.base_atom_count{
                 let new_atom = Atom{
                     position: self.atoms[j].position + self.vec3 * i as f32,
                     number: self.atoms[j].number
@@ -61,7 +59,26 @@ impl UnitCell {
             }
         }
     }
-    pub fn morph(&mut self, a: Vector3<f32>, b: Vector3<f32>, c: Vector3<f32>) {
-        
+    pub fn rotate(&mut self, direction: char, angle: f32){
+        let mut axisangle: Vector3<f32> = Vector3::x();
+        match direction {
+            'x' => axisangle = Vector3::x(),
+            'y' => axisangle = Vector3::y(),
+            'z' => axisangle = Vector3::z(),
+            _ => println!("Defaulting to x")
+        }
+        axisangle = axisangle * angle;
+        let rotation = Rotation3::new(axisangle);
+        self.vec1 = rotation * self.vec1;
+        self.vec2 = rotation * self.vec2;
+        self.vec3 = rotation * self.vec3;
+        for i in 0..self.atoms.len() {
+            self.atoms[i].position = rotation * self.atoms[i].position;
+        }
     }
+    
+    pub fn morph(&mut self, a: Vector3<f32>, b: Vector3<f32>, c: Vector3<f32>) {
+
+    }
+
 }
